b9c7f096caeddd15fddb05e0967315e8f61f3702
Samuel Victor <samuelvsl@hotmail.com>
2016-09-21 19:43:13 -0300

    Adição de problemas resolvidos.

---
+++ 1001.c
@@ -0,0 +1,15 @@
+// AUTOR:     Samuel Victor Silveira de Lima
+// PROBLEMA:  1001 - Extremamente Básico
+// SUBMISSÃO: 05/03/13 00:41:50
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int A,B,x;
+  scanf("%d%d", &A,&B);
+  x=A+B;
+  printf("X = %d\n", x);
+  return 0;
+}
